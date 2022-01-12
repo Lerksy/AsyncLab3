9dddc73908ad3eb736aedaaec1965e4ee55101e0
TheDoctorWhoo <olexa99@gmail.com>
2022-01-12 22:22:33 +0200

    Some space optimization

--- main.cpp
+++ main.cpp
@@ -7,38 +7,32 @@
 
 QVector<int> createArray(size_t size){
     QVector<int> array;
-    for(size_t i = 0; i < size; i++){
-        array.push_back(QRandomGenerator::global()->bounded(100, 8192));
-    }
+    for(size_t i = 0; i < size; i++) array.push_back(QRandomGenerator::global()->bounded(100, 8192));
     return array;
 }
 
 int maxOfArray(QVector<int> array){
     qint64 max = array.at(0);
-    for(auto i:array){
-        if(i > max) max = i;
-    }
+    for(auto i:array) if(i > max) max = i;
     return max;
 }
 
 QVector<int> leaveMoreKMax(QVector<int> array){
     int border = static_cast<int>(maxOfArray(array)*0.7);
-    for(size_t i = 0; i < static_cast<size_t>(array.size()); i++){
+    for(size_t i = 0; i < static_cast<size_t>(array.size()); i++)
         if(array.at(i) <= border){
             array.removeAt(i);
             i--;
         }
-    }
     return array;
 }
 
 QVector<int> leaveAliquotK(QVector<int> array){
-    for(size_t i = 0; i < static_cast<size_t>(array.size()); i++){
+    for(size_t i = 0; i < static_cast<size_t>(array.size()); i++)
         if(array.at(i)%3 != 0){
             array.removeAt(i);
             i--;
         }
-    }
     return array;
 }
 
